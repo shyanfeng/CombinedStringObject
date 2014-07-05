5f183094db99d52ccafb0f09d99ada8656cbbe77
kenmunyap <kenmunyap@gmail.com>
2014-07-06 02:53:12 +0800

    operand1 and operand3 value does not goes in

--- src/FDA.c
+++ src/FDA.c
@@ -8,35 +8,67 @@
 #include "ErrorCode.h"
 
 int FDA(String *arguments){
-	int mask1 = 0xfff;
-	int mask2 = 0xff;
-	int BANKED = 0x1;
-	int F = 0x1;
+	// int mask1 = 0xfff;
+	// int mask2 = 0xff;
+
+	// int F = 0x1;
+	// int ACCESS = 0;
+	// int a;
+	// int operand1,operand2,operand3,maskValue1;
+	// int opcode;
+	
+	
+	int operand1,operand3,check,value;
+	int BANKED = 1;
 	int ACCESS = 0;
-	int a;
-	int operand1,operand2,operand3,maskValue1;
-	int opcode;
-
-	CEXCEPTION_T error;
-	operand1 = extractValue(arguments);
-
-	if(((operand1 & 0x100)>>8) == 1 ){
-		operand1 = operand1&0x7f;
-		operand2 = F<<9;
-		operand3 = BANKED<<9;
-	}else{
-		operand1 = operand1&0x7f;
-		operand2 = F<<9;
-		operand3 = ACCESS<<9;
+	ErrorCode error;
+	ErrorCode e;
+	
+	
+	Try{
+		operand1 = extractValue(arguments);
+		printf("%x \n",operand1);
+	}Catch(error){
+		if(error != ERR_EMPTY_VALUE){
+		   operand1 = operand1&0x7f;
+			Try{
+				operand3 = extractDestination(arguments);
+				printf("%x \n",operand3);
+			}Catch(e){
+				if(e != ERR_EMPTY_VALUE){
+					if(operand3 == 1){
+						printf("aass \n");
+					}else{
+						printf("aaas \n");
+					}
+				}else{
+					printf("aas");
+				}
+			}
+		}else{
+			Throw(ERR_EMPTY_VALUE);
+		}
 	}
 	
 	
+	// here:
+	// check = (operand1 & 0x100)>>8;
+	// if(check == 1){
+		// operand3 = ACCESS;
+	// }else{
+		// operand3 = BANKED;	
+	// }
+	
+	// come:
+	// if(operand3 == 0){
+		// operand3 = ACCESS;
+	// }else{
+		// operand3 = BANKED;
+	// }
 	
 
-	return  operand1+operand2+operand3;
-
+	return  operand1;
 }
-
 
 
 
--- test/test_FDA.c
+++ test/test_FDA.c
@@ -11,17 +11,21 @@
 void setUp(void){}
 void tearDown(void){}
 
-void test_FDA_operand1_is_1(void){
-	int value;
-	Text *new = textNew("0x123");
+void xtest_FDA_operand1_is_NULL(void){
+	int value,error;
+	Text *new = textNew("");
 	String *string = stringNew(new);
 	
-	extractValue_ExpectAndReturn(string,0x123);
-	value = FDA(string);
+	extractValue_ExpectAndThrow(string,3);
 	
 	
-	TEST_ASSERT_EQUAL(value,1059);
-
+	Try{
+		value = FDA(string);
+	}Catch(error){
+		TEST_ASSERT_EQUAL(error,ERR_EMPTY_VALUE);
+		return;
+	}
+	TEST_FAIL_MESSAGE("No exception thrown.");
 }
 
 void test_FDA_operand1_is_0(void){
@@ -29,10 +33,35 @@ void test_FDA_operand1_is_0(void){
 	Text *new = textNew("0x23");
 	String *string = stringNew(new);
 	
+	extractValue_ExpectAndThrow(string,0);
+	extractDestination_ExpectAndThrow(string,0);
+	FDA(string);
 	extractValue_ExpectAndReturn(string,0x23);
-	value = FDA(string);
+	extractDestination_IgnoreAndReturn(1);
+	FDA(string);
 	
+	//FDA(string);
 	
-	TEST_ASSERT_EQUAL(value,547);
+
+
+
+
+//	TEST_ASSERT_EQUAL(value,0x23);
 }
 
+void xtest_FDA_operand1_is_1(void){
+	int value;
+	Text *new = textNew("0x123");
+	String *string = stringNew(new);
+
+	
+	extractValue_ExpectAndThrow(string,0);
+	extractDestination_ExpectAndThrow(string,0);
+	FDA(string);
+	extractValue_ExpectAndReturn(string,0x123);
+	extractDestination_IgnoreAndReturn(1);
+	FDA(string);
+	
+	TEST_ASSERT_EQUAL(value,0);
+
+}
